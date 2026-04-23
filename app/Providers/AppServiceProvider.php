47c4103bb5dbc7174643885eac0b23d82dadc12b
Luis David Pico Pita <pik0hpik0hpik0hpik0h@gmail.com>
2026-04-22 21:17:03 -0500

    13. COMMIT - Correo de confirmación

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+
+            return (new MailMessage)
+                ->subject('Verifica tu cuenta - Mi Estantería')
+                ->greeting('¡Hola '.$notifiable->name.'!')
+                ->line('Gracias por registrarte en Mi Estantería.')
+                ->line('Para activar tu cuenta y comenzar a usar la plataforma, confirma tu correo electrónico.')
+                ->action('Verificar mi cuenta', $url)
+                ->line('Si no creaste esta cuenta, puedes ignorar este mensaje.')
+                ->salutation('Saludos, Equipo Mi Estantería');
+        });
     }
 }
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,13 @@
+@props(['url'])
+
+<tr>
+<td class="header" style="text-align:center;">
+<a href="{{ $url }}" style="display:inline-block;">
+<img 
+    src="https://res.cloudinary.com/danc3cnzs/image/upload/v1776910382/logo_navbar_tyjj5z.png"
+    alt="Mi Estantería"
+    style="max-width:220px; height:auto; display:block;"
+>
+</a>
+</td>
+</tr>
